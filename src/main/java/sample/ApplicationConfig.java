cf23d00349ef4156c281a82ff265f6ce25276cc0
junichiro_kazama <junichiro_kazama@xon.jp>
2017-05-25 10:00:26 +0900

    Spring Boot 1.5.2 to 1.5.3 Gradle 3.4.1 to 3.5

--- src/main/java/sample/ApplicationConfig.java
+++ src/main/java/sample/ApplicationConfig.java
@@ -1,10 +1,8 @@
 package sample;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.*;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * アプリケーションにおけるBean定義を表現します。
@@ -15,25 +13,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 public class ApplicationConfig {
 
-    /** SpringMvcの拡張コンフィギュレーション */
-    @Configuration
-    public static class WebMvcConfig extends WebMvcConfigurerAdapter {
-        @Autowired
-        private MessageSource message;
-
-        /** BeanValidationメッセージのUTF-8に対応したValidator。 */
-        @Bean
-        public LocalValidatorFactoryBean validator() {
-            LocalValidatorFactoryBean factory = new LocalValidatorFactoryBean();
-            factory.setValidationMessageSource(message);
-            return factory;
-        }
-
-        /** 標準Validatorの差し替えをします。 */
-        @Override
-        public org.springframework.validation.Validator getValidator() {
-            return validator();
-        }
+    /** BeanValidationメッセージのUTF-8に対応したValidator。 */
+    @Bean
+    public LocalValidatorFactoryBean mvcValidator(MessageSource message) {
+        LocalValidatorFactoryBean factory = new LocalValidatorFactoryBean();
+        factory.setValidationMessageSource(message);
+        return factory;
     }
 
 }
