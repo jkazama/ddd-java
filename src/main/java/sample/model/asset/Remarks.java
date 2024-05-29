da58616810f90fca6b500026553466b4d3074b0c
jkazama <junichiro_kazama@xon.jp>
2024-05-29 03:58:50 +0000

    Spring Boot 3.1 to 3.3 Java 17 to 21 Gradle 8.1 to 8.7

--- src/main/java/sample/model/asset/Remarks.java
+++ src/main/java/sample/model/asset/Remarks.java
@@ -5,17 +5,11 @@ package sample.model.asset;
  */
 public interface Remarks {
 
-    /** 振込入金 */
     String CASH_IN = "cashIn";
-    /** 振込入金(調整) */
     String CASH_IN_ADJUST = "cashInAdjust";
-    /** 振込入金(取消) */
     String CASH_IN_CANCEL = "cashInCancel";
-    /** 振込出金 */
     String CASH_OUT = "cashOut";
-    /** 振込出金(調整) */
     String CASH_OUT_ADJUST = "cashOutAdjust";
-    /** 振込出金(取消) */
     String CASH_OUT_CANCEL = "cashOutCancel";
 
 }
